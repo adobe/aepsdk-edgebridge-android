0bbcb3644a28ed85e368c6bce820db3ad91278c5
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2022-11-30 13:50:48 -0800

    Remove executor tests

--- code/edgebridge/src/test/java/com/adobe/marketing/mobile/edge/bridge/EdgeBridgeExtensionTests.java
+++ code/edgebridge/src/test/java/com/adobe/marketing/mobile/edge/bridge/EdgeBridgeExtensionTests.java
@@ -13,7 +13,6 @@ package com.adobe.marketing.mobile.edge.bridge;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -822,19 +821,6 @@ public class EdgeBridgeExtensionTests {
 		extension.handleRulesEngineResponse(event);
 
 		verify(mockExtensionApi, never()).dispatch(any(Event.class));
-	}
-
-	// ========================================================================================
-	// getExecutor
-	// ========================================================================================
-	@Test
-	public void testGetExecutor_notNull() {
-		assertNotNull(extension.getExecutor());
-	}
-
-	@Test
-	public void testGetExecutor_singleton() {
-		assertSame(extension.getExecutor(), extension.getExecutor());
 	}
 
 	// ========================================================================================
