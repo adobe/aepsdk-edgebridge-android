b067bba6b3fc3f2d0b07bfa301747803ed7eb8a8
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2022-12-01 12:40:15 -0800

    Reorder vars based on access level

--- code/edgebridge/src/main/java/com/adobe/marketing/mobile/edge/bridge/EdgeBridge.java
+++ code/edgebridge/src/main/java/com/adobe/marketing/mobile/edge/bridge/EdgeBridge.java
@@ -35,6 +35,7 @@ public class EdgeBridge {
 	public static final Class<? extends Extension> EXTENSION = EdgeBridgeExtension.class;
 	private static final String LOG_SOURCE = "EdgeBridge";
 	public static final Class<? extends Extension> EXTENSION = EdgeBridgeExtension.class;
+	private static final String LOG_SOURCE = "EdgeBridge";
 
 	private EdgeBridge() {}
 
