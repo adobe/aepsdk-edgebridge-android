7f6b9599e5d400784b58b18f19b80ddafa383d21
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2023-01-31 15:33:17 -0800

    Update tutorial start app to use core 2.0
    Add spotless formatting to start tutorial app

--- Documentation/tutorials/EdgeBridgeTutorialAppStart/app/src/main/java/com/adobe/marketing/mobile/tutorial/MainActivity.java
+++ Documentation/tutorials/EdgeBridgeTutorialAppStart/app/src/main/java/com/adobe/marketing/mobile/tutorial/MainActivity.java
@@ -1,84 +1,84 @@
 /*
- Copyright 2022 Adobe. All rights reserved.
-
- This file is licensed to you under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License. You may obtain a copy
- of the License at http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software distributed under
- the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- OF ANY KIND, either express or implied. See the License for the specific language
- governing permissions and limitations under the License.
+  Copyright 2022 Adobe. All rights reserved.
+  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software distributed under
+  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+  OF ANY KIND, either express or implied. See the License for the specific language
+  governing permissions and limitations under the License.
 */
+
 package com.adobe.marketing.mobile.tutorial;
+
+import static com.adobe.marketing.mobile.tutorial.MainApp.LOG_TAG;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.adobe.marketing.mobile.Assurance;
-import com.adobe.marketing.mobile.LoggingMode;
 import com.adobe.marketing.mobile.MobileCore;
-
+import com.adobe.marketing.mobile.services.Log;
 import com.adobe.marketing.mobile.tutorial.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String LOG_TAG = "MainActivity";
 
-    private ActivityMainBinding binding;
+	private static final String LOG_SOURCE = "MainActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	private ActivityMainBinding binding;
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // Handle deep linking, and connecting to Assurance
-        final Intent intent = getIntent();
-        final Uri data = intent.getData();
+		binding = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
 
-        if (data != null) {
-            Assurance.startSession(data.toString());
-            MobileCore.log(LoggingMode.VERBOSE, LOG_TAG, "Deep link received " + data.toString());
-        }
+		// Handle deep linking, and connecting to Assurance
+		final Intent intent = getIntent();
+		final Uri data = intent.getData();
 
-        setSupportActionBar(binding.toolbar);
-    }
+		if (data != null) {
+			Assurance.startSession(data.toString());
+			Log.debug(LOG_TAG, LOG_SOURCE, "Deep link received " + data.toString());
+		}
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        MobileCore.lifecyclePause();
-    }
+		setSupportActionBar(binding.toolbar);
+	}
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        MobileCore.setApplication(getApplication());
-        MobileCore.lifecycleStart(null);
-    }
+	@Override
+	public void onPause() {
+		super.onPause();
+		MobileCore.lifecyclePause();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+	@Override
+	public void onResume() {
+		super.onResume();
+		MobileCore.setApplication(getApplication());
+		MobileCore.lifecycleStart(null);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        Intent intent;
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 
-        if (item.getItemId() == R.id.connectToAssurance) {
-            intent = new Intent(MainActivity.this, AssuranceActivity.class);
-            startActivity(intent);
-            return true;
-        }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Intent intent;
 
-        return super.onOptionsItemSelected(item);
-    }
+		if (item.getItemId() == R.id.connectToAssurance) {
+			intent = new Intent(MainActivity.this, AssuranceActivity.class);
+			startActivity(intent);
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
