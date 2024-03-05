529c9e5eac36aa1282f9732e90a13d0a442cd8a2
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2024-03-04 17:51:39 -0800

    Add support for mocks for ServiceProvider and sub-services
    Add property test cases that validate different return values from sub-service APIs result in expected final values

--- code/edgebridge/src/test/java/com/adobe/marketing/mobile/edge/bridge/EdgeBridgePropertiesTests.kt
+++ code/edgebridge/src/test/java/com/adobe/marketing/mobile/edge/bridge/EdgeBridgePropertiesTests.kt
@@ -12,13 +12,21 @@
 package com.adobe.marketing.mobile.edge.bridge
 
 import com.adobe.marketing.mobile.ExtensionApi
+import com.adobe.marketing.mobile.services.AppContextService
+import com.adobe.marketing.mobile.services.AppState
+import com.adobe.marketing.mobile.services.DeviceInforming
+import com.adobe.marketing.mobile.services.ServiceProvider
+import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
+import org.mockito.MockedStatic
 import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class EdgeBridgePropertiesTests {
@@ -27,14 +35,44 @@ class EdgeBridgePropertiesTests {
     @Mock
     lateinit var mockExtensionApi: ExtensionApi
 
+    @Mock
+    private lateinit var mockAppContextService: AppContextService
+
+    @Mock
+    private lateinit var mockDeviceInfoService: DeviceInforming
+
+    @Mock
+    private lateinit var mockServiceProvider: ServiceProvider
+
+    private lateinit var mockedStaticServiceProvider: MockedStatic<ServiceProvider>
+
     // Performs pre-test setup before each test case
     // Resets the mocked ExtensionApi instance mockExtensionApi (handles event dispatch)
     // Creates a new instance of the EdgeBridgeExtension using the ExtensionApi mock
     @Before
     fun setup() {
+        MockitoAnnotations.openMocks(this) // Initialize mocks annotated with @Mock
+
+        // Mock the static method to return mocked ServiceProvider
+        mockedStaticServiceProvider = Mockito.mockStatic(ServiceProvider::class.java)
+        mockedStaticServiceProvider.`when`<Any> { ServiceProvider.getInstance() }.thenReturn(mockServiceProvider)
+
+        // Set up service provider to return mock services
+        whenever(mockServiceProvider.appContextService).thenReturn(mockAppContextService)
+        whenever(mockServiceProvider.deviceInfoService).thenReturn(mockDeviceInfoService)
+
         Mockito.reset(mockExtensionApi)
         extension = EdgeBridgeExtension(mockExtensionApi)
     }
+
+    @After
+    fun teardown() {
+        mockedStaticServiceProvider.close()
+    }
+
+    // region Map modification tests
+    // These tests validate the modification of the input map results in the desired format,
+    // and that the values are correctly populated, without testing what specific values are set.
 
     // Validates method:
     // 1. Creates expected hierarchy when not present:
@@ -158,5 +196,174 @@ class EdgeBridgePropertiesTests {
             )
         )
         assertEquals(expectedData, eventData)
+    }
+
+    // endregion Map modification tests
+
+    // region Properties tests
+    // These tests validate that the property values are set correctly based on various possible
+    // ServiceProvider values.
+
+    // Validates `AppState` `null` is correctly added to the payload
+    @Test
+    fun testAnalyticsProperties_withAppStateNull_addsCorrectValue() {
+        configureAppContextService(null)
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "foreground",
+                    "contextData" to mapOf(
+                        "a.AppID" to "null"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    // Validates `AppState.FOREGROUND` is correctly added to the payload
+    @Test
+    fun testAnalyticsProperties_withAppStateForeground_addsCorrectValue() {
+        configureAppContextService(AppState.FOREGROUND)
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "foreground",
+                    "contextData" to mapOf(
+                        "a.AppID" to "null"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    // Validates `AppState.BACKGROUND` is correctly added to the payload
+    @Test
+    fun testAnalyticsProperties_withAppStateBackground_addsCorrectValue() {
+        configureAppContextService(AppState.BACKGROUND)
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "background",
+                    "contextData" to mapOf(
+                        "a.AppID" to "null"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    @Test
+    fun testAnalyticsProperties_withFullDeviceInfo_addsCorrectValue() {
+        configureDeviceInfoService("Test App Name", "1.2.3", "456")
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "foreground",
+                    "contextData" to mapOf(
+                        "a.AppID" to "Test App Name 1.2.3 (456)"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    @Test
+    fun testAnalyticsProperties_withDeviceInfoAppName_addsCorrectValue() {
+        configureDeviceInfoService("Test App Name", null, null)
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "foreground",
+                    "contextData" to mapOf(
+                        "a.AppID" to "Test App Name"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    @Test
+    fun testAnalyticsProperties_withDeviceInfoAppVersion_addsCorrectValue() {
+        configureDeviceInfoService(null, "1.2.3", null)
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "foreground",
+                    "contextData" to mapOf(
+                        "a.AppID" to "null 1.2.3"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    @Test
+    fun testAnalyticsProperties_withDeviceInfoAppVersionCode_addsCorrectValue() {
+        configureDeviceInfoService(null, null, "456")
+
+        val eventData = mutableMapOf<String, Any>()
+
+        extension.addAnalyticsProperties(eventData)
+
+        val expectedData = mapOf(
+            "__adobe" to mapOf(
+                "analytics" to mapOf(
+                    "cp" to "foreground",
+                    "contextData" to mapOf(
+                        "a.AppID" to "null (456)"
+                    )
+                )
+            )
+        )
+        assertEquals(expectedData, eventData)
+    }
+
+    // endregion Properties tests
+
+    // Private helpers
+    private fun configureAppContextService(appState: AppState?) {
+        whenever(mockAppContextService.appState).thenReturn(appState)
+    }
+
+    private fun configureDeviceInfoService(applicationName: String?, applicationVersion: String?, applicationVersionCode: String?) {
+        whenever(mockDeviceInfoService.applicationName).thenReturn(applicationName)
+        whenever(mockDeviceInfoService.applicationVersion).thenReturn(applicationVersion)
+        whenever(mockDeviceInfoService.applicationVersionCode).thenReturn(applicationVersionCode)
     }
 }
